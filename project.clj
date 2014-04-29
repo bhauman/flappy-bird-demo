ae1bc928f0069ebeed73a3ec4bf3c9644ef8b3e7
Bruce Hauman <bhauman@gmail.com>
2014-04-29 08:33:16 -0400

    live coding flappy bird

---
+++ project.clj
@@ -0,0 +1,23 @@
+(defproject flappy-bird-demo "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2202"]                 
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
+                 [prismatic/schema "0.2.0"]                 
+                 [sablono "0.1.5"]
+                 [figwheel "0.1.0-SNAPSHOT"]]
+
+  :plugins [[lein-cljsbuild "1.0.3"] [lein-figwheel "0.1.0-SNAPSHOT"]]
+
+  :source-paths ["src"]
+
+  :cljsbuild { 
+    :builds [{:id "flappy-bird-demo"
+              :source-paths ["src"]
+              :compiler {
+                :output-to "resources/public/js/flappy_bird_demo.js"
+                :output-dir "resources/public/js/out"
+                :optimizations :none
+                :source-map true}}]})
---
+++ src/flappy_bird_demo/core.cljs
@@ -0,0 +1,228 @@
+(ns flappy-bird-demo.core
+  (:require
+   [sablono.core :as sab :include-macros true]
+   [figwheel.client :as fw :include-macros true]
+   [cljs.core.async :refer [<! chan sliding-buffer put! close! timeout]])
+  (:require-macros
+   [cljs.core.async.macros :refer [go-loop go]]))
+(enable-console-print!)
+
+(defn floor [x] (.floor js/Math x))
+
+(defn translate
+  ([start-pos vel time]
+     (floor (+ start-pos (* time vel))))
+  ([start-pos modulo vel time]
+     (floor (+ start-pos (mod (* time vel) modulo)))))
+
+(def horiz-vel -0.15)
+(def gravity 0.05)
+(def jump-vel 21)
+(def start-y 312)
+(def bottom-y 561)
+(def flappy-x 212)
+(def flappy-width 57)
+(def flappy-height 41)
+(def pillar-spacing 324)
+(def pillar-gap 158) ;; 158
+(def pillar-width 86)
+
+(def starting-state { :timer-running false
+                      :jump-count 0
+                      :initial-vel 0
+                      :start-time 0
+                      :flappy-start-time 0
+                      :flappy-y   start-y
+                      :pillar-list
+                      [{:start-time 0
+                        :pos-x 900
+                        :gap-top 200 }]})
+
+(defn reset-state [{:keys [event-chan] :as st} cur-time]
+  (-> starting-state
+      (update-in [:pillar-list] (fn [pls] (map #(assoc % :start-time cur-time) pls)))
+      (assoc
+          :start-time cur-time
+          :flappy-start-time cur-time
+          :event-chan event-chan
+          :timer-running true)))
+
+(fw/defonce flap-state (atom starting-state))
+
+(defmulti transform first)
+
+(defmethod transform :default [_ state] state)
+
+(defn curr-pillar-pos [cur-time {:keys [pos-x start-time] }]
+  (translate pos-x horiz-vel (- cur-time start-time)))
+
+(defn in-pillar? [{:keys [cur-x]}]
+  (and (>= (+ flappy-x flappy-width)
+           cur-x)
+       (< flappy-x (+ cur-x pillar-width))))
+
+(defn in-pillar-gap? [{:keys [flappy-y]} {:keys [gap-top]}]
+  (and (< gap-top flappy-y)
+       (> (+ gap-top pillar-gap)
+          (+ flappy-y flappy-height))))
+
+(defn bottom-collision? [{:keys [flappy-y]}]
+  (>= flappy-y (- bottom-y flappy-height)))
+
+(defn collision? [{:keys [pillar-list] :as st}]
+  (if (some #(or (and (in-pillar? %)
+                      (not (in-pillar-gap? st %)))
+                 (bottom-collision? st)) pillar-list)
+    (assoc st :timer-running false)
+    st))
+
+(defn new-pillar [cur-time pos-x]
+  {:start-time cur-time
+   :pos-x      pos-x
+   :cur-x      pos-x
+   :gap-top    (+ 60 (rand-int (- bottom-y 120 pillar-gap)))})
+
+(defn update-pillars [{:keys [pillar-list cur-time] :as st}]
+  (let [pillars-with-pos (map #(assoc % :cur-x (curr-pillar-pos cur-time %)) pillar-list)
+        pillars-in-world (sort-by
+                          :cur-x 
+                          (filter #(> (:cur-x %) (- pillar-width)) pillars-with-pos))]
+    (assoc st
+      :pillar-list
+      (if (< (count pillars-in-world) 3)
+        (conj pillars-in-world
+              (new-pillar
+               cur-time
+               (+ pillar-spacing
+                  (:cur-x (last pillars-in-world)))))
+        pillars-in-world))))
+
+
+(defn sine-wave [st]
+  (assoc st
+    :flappy-y
+    (+ start-y (* 30 (.sin js/Math (/ (:time-delta st) 300))))))
+
+(defn update-flappy [{:keys [time-delta initial-vel flappy-y jump-count] :as st}]
+  (if (pos? jump-count)
+    (let [cur-vel (- initial-vel (* time-delta gravity))
+          new-y   (- flappy-y cur-vel)
+          new-y   (if (> new-y (- bottom-y flappy-height))
+                    (- bottom-y flappy-height)
+                    new-y)]
+      (assoc st
+        :flappy-y new-y))
+    (sine-wave st)))
+
+(defn score [{:keys [cur-time start-time] :as st}]
+  (let [score (- (.abs js/Math (floor (/ (- (* (- cur-time start-time) horiz-vel) 544)
+                               pillar-spacing)))
+                 4)]
+  (assoc st :score (if (neg? score) 0 score))))
+
+(defmethod transform :time [[_ timestamp] state]
+  (-> state
+      (assoc
+          :cur-time timestamp
+          :time-delta (- timestamp (:flappy-start-time state)))
+      update-flappy
+      update-pillars
+      collision?
+      #_score))
+
+(defn jump [{:keys [cur-time jump-count] :as state}]
+  (-> state
+      (assoc
+          :jump-count (inc jump-count)
+          :flappy-start-time cur-time
+          :initial-vel jump-vel)))
+
+(defmethod transform :jump [_ state]
+  (jump state))
+
+(defn start-game [event-chan]
+  (.requestAnimationFrame js/window (fn [time]
+                                      (put! event-chan [:start-game time]))))
+
+(defmethod transform :start-game [[_ time] state]
+  (reset-state state time))
+
+;; derivatives
+
+(defn border [{:keys [cur-time] :as state}]
+  (-> state
+      (assoc :border-pos (mod (translate 0 horiz-vel cur-time) 23))))
+
+(defn pillar-offset [{:keys [cur-time]} {:keys [gap-top] :as p}]
+  (assoc p
+    :upper-height gap-top
+    :lower-height (- bottom-y gap-top pillar-gap)))
+
+(defn pillar-offsets [state]
+  (update-in state [:pillar-list]
+             (fn [pillar-list]
+               (map (partial pillar-offset state)
+                    pillar-list))))
+
+(defn world [state]
+  (-> state
+      border
+      pillar-offsets))
+
+(defn px [n] (str n "px"))
+
+(defn pillar [{:keys [cur-x upper-height lower-height]}]
+  [:div.pillars
+   [:div.pillar.pillar-upper {:style {:left (px cur-x)
+                                       :height upper-height}}]
+   [:div.pillar.pillar-lower {:style {:left (px cur-x)
+                                       :height lower-height}}]])
+
+(defn main-template [{:keys [score cur-time jump-count timer-running border-pos event-chan flappy-y pillar-list]}]
+  (sab/html [:div.board { :onMouseDown (fn [e]
+                                         #_(put! event-chan [:jump])
+                                         (swap! flap-state jump)
+                                         (.preventDefault e))}
+             [:h1.score score]
+             (if-not timer-running
+               [:a.start-button {:onClick #(start-game event-chan)}
+                (if (pos? jump-count) "RESTART" "START")]
+               [:span])
+             [:div (map pillar pillar-list)]
+             [:div.flappy {:style {:top (px flappy-y)}}]
+             [:div.scrolling-border {:style { :background-position-x (px border-pos)}}]]))
+
+(defn render-to [react-comp node]
+  (.renderComponent js/React react-comp node))
+
+(let [node (.getElementById js/document "board-area")]
+  (defn renderer [full-state]
+    (render-to (main-template full-state) node)))
+
+(defn start []
+  (let [event-chan (chan (sliding-buffer 1))]
+    (go-loop []
+             (when-let [msg (<! event-chan)]
+               (let [new-state (transform msg @flap-state)]
+                 (reset! flap-state (-clone new-state))
+                 (when (:timer-running new-state)
+                   (<! (timeout 30))
+                   (.requestAnimationFrame js/window (fn [time] (put! event-chan [:time time]))))
+                 (recur))))
+    (swap! flap-state assoc :event-chan event-chan )
+    (add-watch flap-state :renderer (fn [_ _ _ n]
+               (renderer (world n))))
+    (put! event-chan [:time (or (:cur-time @flap-state) 0)])))
+
+(defn stop []
+  (remove-watch flap-state :renderer)
+  (when (:event-chan @flap-state)
+    (close! (:event-chan @flap-state))))
+
+(defn restart []
+  (stop)
+  (start))
+
+(fw/defonce starting-game (start))
+
+(fw/watch-and-reload :jsload-callback (fn [url] (restart)))
